e314c2102656ac68e77de2b4e6a9845be46e187d
bmojek <bartekmojek@gmail.com>
2024-07-06 13:33:50 +0200

    Komunikacja api

--- p1-api/index.ts
+++ p1-api/index.ts
@@ -5,14 +5,25 @@ interface User {
   id: string;
   username: string;
   password: string;
+  email: string;
 }
 
 const db: User[] = [];
 const app = express();
 const port = 3000;
 
-db.push({ id: "1", username: "admin", password: "admin" });
-db.push({ id: "2", username: "wonnski", password: "wonnski" });
+db.push({
+  id: "1",
+  username: "admin",
+  password: "admin",
+  email: "admin@admin.com",
+});
+db.push({
+  id: "2",
+  username: "wonnski",
+  password: "wonnski",
+  email: "wonnski@wonnski.com",
+});
 app.use(cors());
 app.use(express.json());
 
@@ -39,10 +50,19 @@ app.get("/", (req, res) => {
 app.post("/register", (req, res) => {
   const login: string = req.body.login;
   const password: string = req.body.password;
-
+  const email: string = req.body.email;
   const userId: string = new Date().toISOString();
 
-  const newUser: User = { id: userId, username: login, password: password };
+  const existingUser = db.find((user) => user.username === login);
+  if (existingUser) {
+    return res.status(409).send({ message: "Username is already taken" });
+  }
+  const newUser: User = {
+    id: userId,
+    username: login,
+    password: password,
+    email: email,
+  };
   db.push(newUser);
 
   res.status(200).send({ user: newUser });
